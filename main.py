85cb1cd6467eb5c74de64c7cc637281b656f367b
Raaid-Shaheer <sahlraaid52@gmail.com>
2026-02-18 15:43:51 +0530

    Skeleton of project

--- main.py
+++ main.py
@@ -4,13 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+def add_task():
+     pass
+def update_task():
+    pass
+def delete_task():
+    pass
+def Mark_Progress(name):
+    pass
+    # in-progress, done
+def list_tasks():
+    #all, done, not-done, in-progress
+    pass
+def task_tracker():
+    pass
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    task_tracker()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
